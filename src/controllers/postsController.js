f9a4e70321a1abec05ed83d724238f2dc3486968
gabrieldeoa <gabrielaraujo.dev@gmail.com>
2024-11-21 23:09:20 -0300

    upload route created

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import { getAllPosts, savePost } from "../models/postsModel.js";
 
 export async function listAllPosts(_, response) {
@@ -9,6 +10,26 @@ export async function createPost(request, response) {
   try {
     const data = request.body;
     const postCreated = await savePost(data);
+
+    response.status(200).json(postCreated);
+  } catch (error) {
+    console.error(error.message);
+    response.status(500).json({ error: "Falha na requisição." });
+  }
+}
+
+export async function uploadImage(request, response) {
+  try {
+    const data = {
+      description: "",
+      image_url: request.file.originalname,
+      image_alt: "",
+    };
+
+    const postCreated = await savePost(data);
+
+    const imageUpdated = `${process.env.UPLOAD_PATH}${postCreated.insertedId}.png`;
+    fs.renameSync(request.file.path, imageUpdated);
 
     response.status(200).json(postCreated);
   } catch (error) {
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { listAllPosts, createPost } from "../controllers/postsController.js";
+import multer from "multer";
+import {
+  listAllPosts,
+  createPost,
+  uploadImage,
+} from "../controllers/postsController.js";
+
+const upload = multer({ dest: process.env.UPLOAD_PATH });
 
 const routes = (app) => {
   app.use(express.json());
 
   app.get("/posts", listAllPosts);
   app.post("/posts", createPost);
+  app.post("/upload", upload.single("image"), uploadImage);
 };
 
 export default routes;
